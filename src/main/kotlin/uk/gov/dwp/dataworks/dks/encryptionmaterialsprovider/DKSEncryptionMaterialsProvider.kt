fe5facb07ed937aa78e25b97bf5ba5e52ce7ece2
udaykiranchokkam <udaykiranchokkam@digital.uc.dwp.gov.uk>
2019-12-06 12:10:30 +0000

    Materials provider integration with dks

--- src/main/kotlin/uk/gov/dwp/dataworks/dks/encryptionmaterialsprovider/DKSEncryptionMaterialsProvider.kt
+++ src/main/kotlin/uk/gov/dwp/dataworks/dks/encryptionmaterialsprovider/DKSEncryptionMaterialsProvider.kt
@@ -4,11 +4,23 @@ import com.amazonaws.services.s3.model.EncryptionMaterials
 import com.amazonaws.services.s3.model.EncryptionMaterialsProvider
 import org.apache.hadoop.conf.Configurable
 import org.apache.hadoop.conf.Configuration
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Component
+import uk.gov.dwp.dataworks.dks.services.KeyService
+import java.security.KeyPair
+import java.security.spec.PKCS8EncodedKeySpec
+import java.security.spec.X509EncodedKeySpec
 
 /**
  * Class to provide encryption materials to an EMR cluster using DKS.
  */
+@Component
 class DKSEncryptionMaterialsProvider : EncryptionMaterialsProvider, Configurable {
+
+    @Autowired
+    private lateinit var keyService:KeyService
+
+    private val keyFactory = java.security.KeyFactory.getInstance("RSA")
 
     private lateinit var configuration: Configuration
 
@@ -23,10 +35,20 @@ class DKSEncryptionMaterialsProvider : EncryptionMaterialsProvider, Configurable
     override fun refresh() {}
 
     override fun getEncryptionMaterials(): EncryptionMaterials {
-        throw UnsupportedOperationException("RSA Key pair is not initialised.")
+        val dataKeyResult =  keyService.batchDataKey()
+        val publicKey = keyFactory.generatePublic(X509EncodedKeySpec(dataKeyResult.plaintextDataKey.toByteArray()))
+        val keyPair = KeyPair(publicKey,null)
+        return EncryptionMaterials(keyPair)
+            .addDescription("keyid",dataKeyResult.dataKeyEncryptionKeyId)
+            .addDescription("encryptedKey",dataKeyResult.ciphertextDataKey)
     }
 
     override fun getEncryptionMaterials(materialsDescription: MutableMap<String, String>?): EncryptionMaterials {
-        throw Exception("Yet to be implemented")
+        val keyId = materialsDescription?.get("keyid")
+        val encryptedDataKey = materialsDescription?.get("encryptedKey")
+        val privateKey = keyService.decryptKey(keyId!!, encryptedDataKey!!)
+        val symmetricKey = keyFactory.generatePrivate(PKCS8EncodedKeySpec(privateKey.toByteArray()))
+        val keyPair = KeyPair(null,symmetricKey)
+        return EncryptionMaterials(keyPair)
     }
 }
