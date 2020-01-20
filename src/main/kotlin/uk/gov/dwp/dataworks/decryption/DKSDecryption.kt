0c9e813f8fa1cd5b89b46f8d51fc7208817d7dde
udaychokkam <udayjavauk@gmail.com>
2020-01-20 19:26:11 +0000

    Before changing to S3 mock

--- src/main/kotlin/uk/gov/dwp/dataworks/decryption/DKSDecryption.kt
+++ src/main/kotlin/uk/gov/dwp/dataworks/decryption/DKSDecryption.kt
@@ -6,10 +6,12 @@ import com.amazonaws.services.s3.model.ObjectMetadata
 import com.amazonaws.services.s3.model.S3Object
 import com.amazonaws.services.s3.model.S3ObjectSummary
 import org.apache.spark.SparkConf
+import org.apache.spark.api.java.JavaRDD
 import org.apache.spark.api.java.JavaSparkContext
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import uk.gov.dwp.dataworks.dks.client.DKSClient
+import uk.gov.dwp.dataworks.dks.client.DKSClientImpl
 import uk.gov.dwp.dataworks.s3.S3Client
 
 object DKSDecryption {
@@ -18,8 +20,7 @@ object DKSDecryption {
     private const val CIPHERTEXT = "cipherText"
     private const val DATAKEYENCRYPTIONID = "datakeyencryptionkeyid"
 
-    @JvmStatic
-    fun main(args: Array<String>) {
+    fun main(args: Array<String>): JavaRDD<List<String>>? {
         val bucketName = args[0]
         val prefix = args[1]
         val region = args[2]
@@ -27,32 +28,36 @@ object DKSDecryption {
         val sc = getSparkContext()
         val s3Client = getS3Client(region)
         val summaries: List<S3ObjectSummary> = s3Client.listObjectsV2(bucketName, prefix).objectSummaries
-        val rdd = sc.parallelize(summaries).map { it.key }.mapPartitions {
-            val objectSummaries = mutableListOf<String>()
-            while (it.hasNext()) {
-                val key = it.next()
-                val s3ClientInExecutor = S3Client.amazonS3(region)
-                val metadata = s3ClientInExecutor.getObjectMetadata(bucketName, key)
-                val s3Object = s3ClientInExecutor.getObject(bucketName, key)
-                objectSummaries.add(decrypt(metadata, s3Object))
-            }
-            objectSummaries.iterator()
-        }
-        val decrypted = rdd.map { it.split("\n") }
-        logger.info("decrypted top 2 " + decrypted.take(2))
+        val summariesRdd = sc.parallelize(summaries)
+        val decryptedRdd = decryptRdd(summariesRdd, region, bucketName)
+        return  decryptedRdd?.map { it.split("\n") }
     }
 
-    private fun getS3Client(region: String): AmazonS3 {
+    fun decryptRdd(summariesRdd: JavaRDD<S3ObjectSummary>, region: String, bucketName: String): JavaRDD<String>? {
+        val rdd = summariesRdd.map { it.key }.mapPartitions {
+            val decryptedStrings = mutableListOf<String>()
+            val s3ClientInExecutor = getS3Client(region)
+            while (it.hasNext()) {
+                val key = it.next()
+                val metadata = s3ClientInExecutor.getObjectMetadata(bucketName, key)
+                val s3Object = s3ClientInExecutor.getObject(bucketName, key)
+                decryptedStrings.add(decrypt(metadata, s3Object))
+            }
+            decryptedStrings.iterator()
+        }
+        return rdd
+    }
+
+    fun getS3Client(region: String): AmazonS3 {
         return  S3Client.amazonS3(region)
     }
 
     fun getSparkContext(): JavaSparkContext {
-        val conf = SparkConf().setAppName("s3-data-decryption")
-        val sc = JavaSparkContext(conf)
-        return sc
+        val conf = SparkConf().setAppName("s3-data-decryption").setMaster("local")
+            return JavaSparkContext(conf)
     }
 
-    private fun decrypt(metadata: ObjectMetadata, s3Object: S3Object): String {
+    fun decrypt(metadata: ObjectMetadata, s3Object: S3Object): String {
 
         val iv = metadata.userMetadata[IV]
         val cipherText = metadata.userMetadata[CIPHERTEXT]
@@ -62,9 +67,14 @@ object DKSDecryption {
         logger.info("Metadata for the key $key :: iv: $iv cipherText: $cipherText dataencryptionkeid: $datakeyEncryptionkeyId")
 
         logger.debug("Calling DKS to decrypt key")
-        val decryptedKey = DKSClient.decryptKey(datakeyEncryptionkeyId!!, cipherText!!)
+        val dksClient = getDKSClient()
+        val decryptedKey = dksClient.decryptKey(datakeyEncryptionkeyId!!, cipherText!!)
         logger.debug("DKS decrypted key successfully!")
 
         return AESCipherService.decrypt(decryptedKey, iv!!, content)
+    }
+
+    fun getDKSClient(): DKSClient {
+        return DKSClientImpl
     }
 }
--- src/test/kotlin/uk/gov/dwp/dataworks/decryption/DKSDecryptionTest.kt
+++ src/test/kotlin/uk/gov/dwp/dataworks/decryption/DKSDecryptionTest.kt
@@ -1,14 +1,75 @@
 package uk.gov.dwp.dataworks.decryption
 
+import com.amazonaws.services.s3.AmazonS3
+import com.amazonaws.services.s3.model.ObjectMetadata
+import com.amazonaws.services.s3.model.S3Object
+import com.amazonaws.services.s3.model.S3ObjectInputStream
+import com.amazonaws.services.s3.model.S3ObjectSummary
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.spyk
 import org.junit.jupiter.api.Test
+import uk.gov.dwp.dataworks.dks.client.DKSClient
 
 
 class DKSDecryptionTest {
 
     @Test
     fun testMain(){
-        val dksDecryption = mockkObject(DKSDecryption)
-        every { dksDecryption } returns "Expected Output"
+        val array = arrayOf("bucket", "prefix", "eu-west-2")
+       // val conf = SparkConf().setAppName("s3-data-decryption-test").setMaster("local")
+       // val localSparkContext =  JavaSparkContext(conf)
+        val dksDecryption = spyk(DKSDecryption)
+        val mockS3Client : AmazonS3 = mockk()
+        val objectSummary1 = S3ObjectSummary()
+        objectSummary1.key = "key1"
+        val objectSummary2 = S3ObjectSummary()
+        objectSummary2.key = "key2"
+
+        val objectMetadata1 = ObjectMetadata()
+        objectMetadata1.userMetadata.put("iv", "iv")
+        objectMetadata1.userMetadata.put("cipherText", "cipherText")
+        objectMetadata1.userMetadata.put("datakeyencryptionkeyid", "datakeyencryptionkeyid")
+
+        val objectMetadata2 = ObjectMetadata()
+        objectMetadata2.userMetadata.put("iv", "iv")
+        objectMetadata2.userMetadata.put("cipherText", "cipherText")
+        objectMetadata2.userMetadata.put("datakeyencryptionkeyid", "datakeyencryptionkeyid")
+
+        val s3Object1 = S3Object()
+        s3Object1.bucketName = array[0]
+        s3Object1.key = "key1"
+        s3Object1.objectContent =  S3ObjectInputStream("sampleText1".toByteArray().inputStream(), null)
+
+        val s3Object2 = S3Object()
+        s3Object2.bucketName = array[0]
+        s3Object2.key = "key2"
+        s3Object2.objectContent = S3ObjectInputStream("sampleText2".toByteArray().inputStream(), null)
+
+
+
+        val summaries = mutableListOf<S3ObjectSummary>()
+        summaries.add(objectSummary1)
+        summaries.add(objectSummary2)
+
+        val mockDKSClient: DKSClient = mockk()
+
+
+        every { mockS3Client.listObjectsV2(array[0], array[1]).objectSummaries } returns summaries
+
+        every { dksDecryption.getS3Client(array[2]) } returns  mockS3Client
+        every { dksDecryption.getS3Client(array[2]) } returns  mockS3Client
+        every { mockS3Client.getObjectMetadata(array[0], "key1") } returns objectMetadata1
+        every { mockS3Client.getObjectMetadata(array[0], "key2") } returns objectMetadata2
+
+        every { mockS3Client.getObject(array[0], "key1") } returns s3Object1
+        every { mockS3Client.getObject(array[0], "key2") } returns s3Object2
+
+        every { dksDecryption.getDKSClient() } returns mockDKSClient
+        every { mockDKSClient.decryptKey("", "") } returns ""
+
+       // every { dksDecryption.getSparkContext() } returns localSparkContext
+        dksDecryption.main(array)
 
     }
 }
